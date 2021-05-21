c5deb0dbc0a0fe3fe45c7a7008ed0190e4cabe0d
= <sky81219@naver.com>
2021-05-21 14:02:02 +0900

    automatic start

--- main.py
+++ main.py
@@ -1,59 +1,54 @@
 import pymysql
-import time
 import sys
-import threading
+import time
 
-import numpy as np
+from collections import deque
 
 """
 각 조명에 대한 평균값 구하기 (알고리즘 설계)
 """
 
+
 # 5초 마다
 # 5분간 데이터 평균
+def data_information():
+    conn = pymysql.connect(host='localhost', user='root', port=3306,
+                           password='0000', db='curtain', charset='utf8')
+    cursor = conn.cursor()
 
-conn = pymysql.connect(host='localhost', user='root', port=3306,
-                       password='0000', db='curtain', charset='utf8')
-cursor = conn.cursor()
+    sql = "SELECT * FROM brightness"
 
-sql = "SELECT * FROM brightness"
+    cursor.execute(sql)
+    res = cursor.fetchall()
+    conn.commit()
+    conn.close()
 
-cursor.execute(sql)
-res = cursor.fetchall()
-conn.commit()
-conn.close()
-
-inside = [data[1] for data in res]
-outside = [data[2] for data in res]
-
-in_average = np.array(inside).mean()
-out_average = np.array(outside).mean()
-
-lux_standard = [0, 100, 200, 300, 400, 500]
+    return res
 
 
-class AverageLight():
+class AverageLight:
     def __init__(self):
-        self.inside_light = in_average
-        self.outside_light = out_average
-        self.hope_list = lux_standard[int(sys.argv[1])]
+        self.inside_light = [data[1] for data in data_information()]
+        self.outside_light = [data[2] for data in data_information()]
+        self.hope_list = int(sys.argv[1])
 
     def control_function(self):
-        print(f"linear average --> {self.inside_light}")
-        print(f"outline average --> {self.outside_light}")
+        print(f"inside_right --> {self.inside_light}")
+        print(f"outside_right --> {self.outside_light}")
+        # curtain
+        for data in self.inside_light:
+            deque_data = deque([])
+            deque_data.append(data)
+            print(deque_data)
 
-        if self.inside_light < self.outside_light:
-            if self.inside_light == self.hope_list:
-                print("LED|0.999")
-            else:
-                print("LED trun off!")
-        else:
-            if self.inside_light == self.hope_list:
-                print("OK")
-            elif self.hope_list > self.inside_light:
-                print("curtain up")
-            else:
-                print("curtain down")
+            while deque_data:
+                if data > self.hope_list:
+                    for i in range(6):
+                        time.sleep(1)
+                        print(f"curtain 단계를 {i}단계까지 올립니다.")
+                        continue
+                elif self.inside_light == self.hope_list:
+                    break
 
 
 AverageLight().control_function()
